cbb8ea8b0275992e744c06aa78c2d007624b5a04
HasanSho <53087417+HasanSho@users.noreply.github.com>
2019-10-03 14:23:19 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -149,12 +149,24 @@ fillRestaurantsHTML = (restaurants = self.restaurants) => {
 createRestaurantHTML = (restaurant) => {
   const li = document.createElement('li');
 
+  const link = document.createElement('a');
+  link.href = DBHelper.urlForRestaurant(restaurant);
+  link.className = restaurant.cuisine_type.toLowerCase();
+  link.setAttribute('aria-label', 'info about ' + restaurant.name + ' restaurant, ' + restaurant.neighborhood);
+  link.tabIndex = '0';
+  li.append(link);
+	
   const image = document.createElement('img');
   image.className = 'restaurant-img';
  // image.alt = "An image for " + DBHelper.nameForRestaurant(restaurant);
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
   li.append(image);
-
+  link.append(image);
+	
+	  const label = document.createElement('div');
+  label.className = 'restaurant-label';
+  link.append(label);
+	
   const name = document.createElement('h1');
   name.innerHTML = restaurant.name;
   li.append(name);
