b6ec7ec2402ce244db9e94e1920e4fadef099c36
HasanSho <53087417+HasanSho@users.noreply.github.com>
2019-10-04 02:21:26 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -26,7 +26,6 @@ self.addEventListener('install',function(event){
 				'img/8.jpg',
 				'img/9.jpg',
 				'img/10.jpg',
-				'img/favicon.png',
 				'https://unpkg.com/leaflet@1.3.1/dist/leaflet.css',
 				'https://unpkg.com/leaflet@1.3.1/dist/leaflet.js'
      		
