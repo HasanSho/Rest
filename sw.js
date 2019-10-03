e40b0a372fdf9395fcd59ed6546e3fd3d565d3b1
HasanSho <53087417+HasanSho@users.noreply.github.com>
2019-10-03 13:33:37 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -17,7 +17,7 @@ self.addEventListener('install',function(event){
 				'./js/dbhelper.js',
 				'./js/main.js',
 				'./js/restaurant_info.js',
-				'./restaurant-review-app/img/1.jpg',
+				'/img/1.jpg',
 				'./img/2.jpg',
 				'./img/3.jpg',
 				'./img/4.jpg',
