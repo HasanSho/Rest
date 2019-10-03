523abe6885aa18312842b57801a3cce16531657d
HasanSho <53087417+HasanSho@users.noreply.github.com>
2019-10-03 18:03:05 +0300

    Update restaurant_info.js

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -134,19 +134,23 @@ fillReviewsHTML = (reviews = self.restaurant.reviews) => {
 createReviewHTML = (review) => {
   const li = document.createElement('li');
   const name = document.createElement('p');
+  name.classList.add('name')
   name.innerHTML = review.name;
   li.appendChild(name);
 
   const date = document.createElement('p');
   date.innerHTML = review.date;
+  date.classList.add('date')
   li.appendChild(date);
 
   const rating = document.createElement('p');
   rating.innerHTML = `Rating: ${review.rating}`;
+  rating.classList.add('rating')
   li.appendChild(rating);
 
   const comments = document.createElement('p');
   comments.innerHTML = review.comments;
+  comments.classList.add('comments')
   li.appendChild(comments);
 
   return li;
