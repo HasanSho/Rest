14854cdc51d79fe8137e7105f5c0252592711a6c
HasanSho <53087417+HasanSho@users.noreply.github.com>
2019-10-05 06:38:48 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,8 +4,18 @@ self.addEventListener('install', function(event) {
 	event.waitUntil(
 		caches.open(staticCacheName).then(function(cache) {
 			return cache.addAll([
-				'./index.html',
-				'./restaurant.html',
+				  './',
+  'index.html',
+  'restaurant.html?id=1',
+  'restaurant.html?id=2',
+  'restaurant.html?id=3',
+  'restaurant.html?id=4',
+  'restaurant.html?id=5',
+  'restaurant.html?id=6',
+  'restaurant.html?id=7',
+  'restaurant.html?id=8',
+  'restaurant.html?id=9',
+  'restaurant.html?id=10',
 				'./css/styles.css',
 				'./data/restaurants.json',
 				'./js/dbhelper.js',
